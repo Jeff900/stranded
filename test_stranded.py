a03ffb481e0a489beb48a528882a2baa351b1cfb
Jeff900 <jeffrey.spruit@gmail.com>
2025-01-24 22:51:50 +0100

    added test file

---
+++ test_stranded.py
@@ -0,0 +1,6 @@
+from stranded import Game
+
+def test_gameclass():
+    game = Game('test.db')
+    # Check if the database is initializes empty, but contains a schema.
+    assert game.check_database() is None
