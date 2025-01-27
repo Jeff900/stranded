a43a9526487b0b33fa1ab61835211981b8c78bd1
Jeff900 <jeffrey.spruit@gmail.com>
2025-01-27 14:13:33 +0100

    minor adjustment to assert statement

--- test_stranded.py
+++ test_stranded.py
@@ -3,4 +3,4 @@ from stranded import Game
 def test_gameclass():
     game = Game('test.db')
     # Check if the database is initializes empty, but contains a schema.
-    assert game.check_database() is None
+    assert game.check_database() is False
